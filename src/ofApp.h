5ea480d569999e20c2a8c3dede2e17bfc4cd10a5
Koen Van der Auwera <koen.vanderauwera@gmail.com>
2018-11-18 22:50:46 +0100

    second attempt at commiting changes

---
+++ src/ofApp.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include "ofMain.h"
+#include "Star.h"
+<<<<<<< HEAD
+#include "ofxGui.h"
+
+class ofApp : public ofBaseApp{
+
+public:
+	void setup();
+	void update();
+	void draw();
+
+	void keyPressed(int key);
+	void keyReleased(int key);
+	void mouseMoved(int x, int y );
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased(int x, int y, int button);
+	void mouseEntered(int x, int y);
+	void mouseExited(int x, int y);
+	void windowResized(int w, int h);
+	void dragEvent(ofDragInfo dragInfo);
+	void gotMessage(ofMessage msg);
+
+	ofxPanel gui;
+	ofParameter<int> framerate;
+	void framerateChanged(int & circleResolution);
+
+=======
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void mouseEntered(int x, int y);
+		void mouseExited(int x, int y);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+		
+>>>>>>> 11a670564e35367c1eef59f108ea748e06f9fa66
+private:
+	std::vector<Star *> stars_;
+};
