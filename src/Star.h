ff6c24bc170bd9829aebb6af767c97b504e5cc65
Koen Van der Auwera <koen.vanderauwera@gmail.com>
2018-11-23 21:23:01 +0100

    9. Gui gekoppeld (aantal sterren, kleur, stdev voor de locatie, snelheid)

--- src/Star.h
+++ src/Star.h
@@ -1,38 +1,26 @@
 #pragma once
 
-<<<<<<< HEAD
 #include "ofxGui.h"
 
-=======
->>>>>>> 11a670564e35367c1eef59f108ea748e06f9fa66
 class Star
 {
 public:
 	Star();
 	~Star();
 
-<<<<<<< HEAD
 	void setup();
 	void draw();
 	void update();
 
 	static ofParameter<ofColor> color_;
+	static ofParameter<float> speed_;
+	static ofParameter<float> radius_step_;
 	static ofParameter<float> dev_;
 
-private:
+// private:
 
 	void init(float xpos, float ypos, float radius);
 
-=======
-	void draw();
-	void step();
-
-	void initRandom();
-	void init(float xpos, float ypos, float radius);
-
-private:
-
->>>>>>> 11a670564e35367c1eef59f108ea748e06f9fa66
 	int star_id_;
 
 	int step_;
@@ -41,9 +29,8 @@ private:
 	float radius_;
 	float xstep_;
 	float ystep_;
-	float radius_step_;
 
-	float give_random(float min_val, float max_val);
+	float give_random(float min_val, float max_val, float dev);
 };
 
 
--- src/ofApp.h
+++ src/ofApp.h
@@ -2,7 +2,6 @@
 
 #include "ofMain.h"
 #include "Star.h"
-<<<<<<< HEAD
 #include "ofxGui.h"
 
 class ofApp : public ofBaseApp{
@@ -23,33 +22,13 @@ public:
 	void windowResized(int w, int h);
 	void dragEvent(ofDragInfo dragInfo);
 	void gotMessage(ofMessage msg);
+	
+	ofxPanel gui_;
+	ofParameter<int> framerate_ = 60;
+	ofParameter<int> numberOfStars_ = defaultNumberOfStars;
+	void numberOfStarsChanged(int & numberOfStars);
 
-	ofxPanel gui;
-	ofParameter<int> framerate;
-	void framerateChanged(int & circleResolution);
-
-=======
-
-class ofApp : public ofBaseApp{
-
-	public:
-		void setup();
-		void update();
-		void draw();
-
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		
->>>>>>> 11a670564e35367c1eef59f108ea748e06f9fa66
 private:
-	std::vector<Star *> stars_;
+	enum { minNumberOfStars = 1, defaultNumberOfStars = 3, maxNumberOfStars = 200 };
+	Star stars_[maxNumberOfStars];
 };
